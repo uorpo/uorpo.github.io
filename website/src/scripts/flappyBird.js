8562d5d546f3ba6e862de3d3c4f8e0778e8fae2c
uorpo <lena.gerlach@rocketmail.com>
2022-01-04 21:02:05 +0100

    Initial commit

--- website/src/scripts/flappyBird.js
+++
@@ -1,7 +0,0 @@
-var pipe = document.getElementById("pipe");
-var hole = document.getElementById("hole");
-
-hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random() * 600) + 150);
-    hole.style.top = random + "px";
-});
