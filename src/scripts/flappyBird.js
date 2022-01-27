c2c0c89253adf8b2c6e3939b17fae6131232d124
uorpo <lena.gerlach@rocketmail.com>
2022-01-27 17:32:38 +0100

    Update flappyBird.js

--- src/scripts/flappyBird.js
+++ src/scripts/flappyBird.js
@@ -88,6 +88,7 @@ function mobile_version() {
   if (isMobile.any()) {
     if (this == tiltButton) {
       mode = "tilt";
+      getAccel();
     } else {
       mode = "blow";
       listenToMicrophone();
@@ -100,7 +101,6 @@ function mobile_version() {
 }
 
 function web_version() {
-  console.log("hhh");
   if (isMobile.any() == null) {
     mode = "click";
     playButton.classList.add("play-allowed");
@@ -145,9 +145,7 @@ function startGame() {
   hole.addEventListener('animationiteration', holeEventListener);
   pipe.style.animation = "pipe 3s infinite linear";
   hole.style.animation = "pipe 3s infinite linear";
-  if (mode != "tilt") {
-    birdFalling();
-  }
+  birdFalling();
   birdJumping();
 };
 
@@ -181,7 +179,7 @@ function jump() {
 }
 
 function birdFalling() {
-  if (mode == "tilt" || mode == null)  {return;}
+  if (mode == null)  {return;}
   birdFallingInterval = setInterval(function() {
     var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
     if (jumping == 0) {
@@ -215,6 +213,78 @@ function checkGameOver() {
            playing = false;
            alert("G a m e  o v e r");
     //window.location.reload();
+  }
+}
+
+function deviceOrientationListener(event) {
+  // Expose each orientation angle in a more readable way
+  rotation_degrees = event.alpha;
+  frontToBack_degrees = event.beta;
+  leftToRight_degrees = event.gamma;
+
+  if (frontToBack_degrees > 10) {
+    jump();
+  }
+
+  // // Update velocity according to how tilted the phone is
+  // // Since phones are narrower than they are long, double the increase to the x velocity
+  // vx = vx + leftToRight_degrees * updateRate * 2;
+  // vy = vy + frontToBack_degrees * updateRate;
+
+  // // Update position and clip it to bounds
+  // px = px + vx * 0.1;
+  // if (px > 50 || px < 0) {
+  //   px = Math.max(0, Math.min(50, px)); // Clip px between 0-98
+  //   vx = 0;
+  // }
+
+  // py = py + vy * 0.1;
+  // if (py > 70 || py < 0) {
+  //   py = Math.max(0, Math.min(70, py)); // Clip py between 0-98
+  //   vy = 0;
+  // }
+
+  // newPosY = frontToBack_degrees * 10;
+  // if (newPosY > 100 || newPosY < -100) {
+  //   newPosY = Math.max(-100, Math.min(100, newPosY));
+  // }
+  // newPosX = leftToRight_degrees * 10;
+  // if (newPosX > 100 || newPosX < -100) {
+  //   newPosX = Math.max(-100, Math.min(100, newPosX));
+  // }
+
+  // dot = document.getElementsByClassName("indicatorDot")[0];
+  // dot.setAttribute(
+  //   "style",
+  //   "left: calc(50% - 120px + " +
+  //     newPosX +
+  //     "px);" +
+  //     "top: calc(40% + 5px + " +
+  //     newPosY +
+  //     "px);"
+  // );
+  // center = document.getElementById("scaleCenter");
+  // var rect = center.getBoundingClientRect();
+  // var dotPos = dot.getBoundingClientRect();
+
+  if (
+    Math.abs(rect.top - dotPos.top) < SCALE_TOLERANCE &&
+    Math.abs(rect.right - dotPos.right) < SCALE_TOLERANCE &&
+    Math.abs(rect.bottom - dotPos.bottom) < SCALE_TOLERANCE &&
+    Math.abs(rect.left - dotPos.left) < SCALE_TOLERANCE
+  ) {
+    dot.setAttribute(
+      "style",
+      "left: calc(50% - 120px + " +
+        newPosX +
+        "px);" +
+        "top: calc(40% + 5px + " +
+        newPosY +
+        "px); background-color: green;"
+    );
+    // alert(
+    //   `Center: ${rect.top}, ${rect.right}, ${rect.bottom}, ${rect.left} \n Dot: ${dotPos.top}, ${dotPos.right}, ${dotPos.bottom}, ${dotPos.left}`
+    // );
   }
 }
 
